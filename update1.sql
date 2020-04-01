333f3ba4238a827984dd5fe6787ceb20ab86e87f
wangyfb <48355746@qq.com>
2020-04-01 21:57:28 +0800

    1

--- update1.sql
+++ update1.sql
@@ -1,3 +1,5 @@
 ﻿INSERT INTO [FapMenu]( [Fid], [MenuCode], [MenuName], [Icon], [MenuUrl], [MenuOrder], [ModuleUid], [ActiveFlag], [Pid], [OrgUid], [GroupUid], [EnableDate], [DisableDate], [Dr], [Ts], [CreateBy], [CreateName], [CreateDate], [UpdateBy], [UpdateName], [UpdateDate], [BadgePlusClass], [ProductUid])
   VALUES( '694174901174534144', '09803', '总裁图表', '', '~/System/Report/CEOChart', 3, 'f4ce11e59cad54031d32', 1, '', '', '', '2020-03-30 21:21:16', '9999-12-31 23:59:59', 0, 694174983642939392, '3521928112763830273', 'wangyanfei', '2020-03-30 21:21:17', '3521928112763830273', 'wangyanfei', '2020-03-30 21:21:27', '', 'FAP')
 GO
+
+https://www.cnblogs.com/rui1236/p/12092775.html
