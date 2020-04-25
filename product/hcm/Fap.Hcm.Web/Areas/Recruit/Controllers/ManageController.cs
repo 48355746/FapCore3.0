4bdc79e652ee553a1adc47d7354e5a0dcb63e210
wangyfb <48355746@qq.com>
2020-04-25 17:40:38 +0800

    1

--- product/hcm/Fap.Hcm.Web/Areas/Recruit/Controllers/ManageController.cs
+++ product/hcm/Fap.Hcm.Web/Areas/Recruit/Controllers/ManageController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Fap.AspNetCore.Infrastructure;
 using Fap.AspNetCore.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using Fap.Core.Extensions;
+using Ardalis.GuardClauses;
 
 namespace Fap.Hcm.Web.Areas.Recruit.Controllers
 {
@@ -16,8 +18,11 @@ namespace Fap.Hcm.Web.Areas.Recruit.Controllers
         }
         public IActionResult Demand()
         {
+            var cols= _dbContext.Columns("RcrtDemand").Where(c => !c.ColProperty.EqualsWithIgnoreCase("3"))?.Select(c=>c.ColName);
+            Guard.Against.Null(cols, nameof(cols));
             JqGridViewModel model = this.GetJqGridModel("RcrtDemand", (qs) =>
             {
+                qs.QueryCols =string.Join(',',cols);
                 qs.GlobalWhere = "BillStatus='PASSED'";
             });
             return View(model);
