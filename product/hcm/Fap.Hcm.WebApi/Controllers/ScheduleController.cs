02d67bbf24dcba490ea5bbda02803127d8684623
王燕飞 <wangyfb@hotmail.com>
2020-06-28 11:00:21 +0800

    webapi

--- product/hcm/Fap.Hcm.WebApi/Controllers/ScheduleController.cs
+++ product/hcm/Fap.Hcm.WebApi/Controllers/ScheduleController.cs
@@ -23,17 +23,6 @@ namespace Fap.Hcm.WebApi.Controllers
             _logger = logger;
         }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+       
     }
 }
