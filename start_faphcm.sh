b40b79ebef0d76160de56acabfb0986fc7eac3ff
wangyfb <wangyfb@vip.163.com>
2020-08-11 15:04:01 +0800

    docker build

--- start_faphcm.sh
+++ start_faphcm.sh
@@ -1,20 +1,15 @@
 #cd fapcore
 cd /var/lib/jenkins-home/workspace/fapcore30
 #stop web container [fapcorehcm]
-#sudo docker stop fapcorehcm
-sudo docker stop fapcorehcmapi
+sudo docker stop fapcorehcm
 #remove web container [fapcorehcm]
-#sudo docker rm -f fapcorehcm
-sudo docker rm -f fapcorehcmapi
+sudo docker rm -f fapcorehcm
 #remove web image [fapcore/hcm]
-#sudo docker rmi -f fapcore/hcm
-sudo docker rmi -f fapcore/hcmapi
+sudo docker rmi -f fapcore/hcm
 #build web image [fapcore/hcm]
 sudo docker build --pull -t fapcore/hcm -f product/hcm/Fap.Hcm.Web/Dockerfile .
 #docker run container,数据卷如下：-v 时区,-v logs,-v 附件。说明：设置appsetting.json日志路径为LogPath（/var/fapcore/logs），设置附件路径为（/var/fapcore/annex）
 sudo docker run --name fapcorehcm -d -p 5000:80 -p 5001:443 -v /etc/localtime:/etc/localtime -v /usr/docker/fapcorehcm/logs:/var/fapcore/logs -v /usr/docker/fapcorehcm/annex:/var/fapcore/annex fapcore/hcm
-#sudo docker build --pull -t fapcore/hcmapi -f product/hcm/Fap.Hcm.WebApi/Dockerfile .
-#sudo docker run --name fapcorehcmapi -d -p 5002:80 -p 5003:443 -v /etc/localtime:/etc/localtime -v /usr/docker/fapcorehcm/logs:/var/fapcore/logs -v /usr/docker/fapcorehcm/annex:/var/fapcore/annex fapcore/hcmapi
 #rm Tag<none> images
 #docker rmi $(docker images | awk '/^<none>/ { print $3 }')
 
@@ -23,3 +18,17 @@ sudo docker run --name fapcorehcm -d -p 5000:80 -p 5001:443 -v /etc/localtime:/e
 #docker-compose up
 #docker-compose -f docker-compose.yml -f docker-compose.override.yml build
 #docker-compose up
+
+#webapi docker build
+#cd fapcore
+#cd /var/lib/jenkins-home/workspace/fapcore30
+#stop web container [fapcorehcmapi]
+#sudo docker stop fapcorehcmapi
+#remove web container [fapcorehcmapi]
+#sudo docker rm -f fapcorehcmapi
+#remove web image [fapcorehcmapi]
+#sudo docker rmi -f fapcore/hcmapi
+#build web image [fapcore/hcmapi]
+#docker run container,数据卷如下：-v 时区,-v logs,-v 附件。说明：设置appsetting.json日志路径为LogPath（/var/fapcore/logs），设置附件路径为（/var/fapcore/annex）
+#sudo docker build --pull -t fapcore/hcmapi -f product/hcm/Fap.Hcm.WebApi/Dockerfile .
+#sudo docker run --name fapcorehcmapi -d -p 5002:80 -p 5003:443 -v /etc/localtime:/etc/localtime -v /usr/docker/fapcorehcm/logs:/var/fapcore/logs -v /usr/docker/fapcorehcm/annex:/var/fapcore/annex fapcore/hcmapi
