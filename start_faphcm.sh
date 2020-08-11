75dd88015597d5da2f06862992d37a823a6d05b4
wangyfb <wangyfb@vip.163.com>
2020-08-11 13:48:33 +0800

    docker

--- start_faphcm.sh
+++ start_faphcm.sh
@@ -10,11 +10,11 @@ sudo docker rm -f fapcorehcmapi
 #sudo docker rmi -f fapcore/hcm
 sudo docker rmi -f fapcore/hcmapi
 #build web image [fapcore/hcm]
-#sudo docker build --pull -t fapcore/hcm -f product/hcm/Fap.Hcm.Web/Dockerfile .
+sudo docker build --pull -t fapcore/hcm -f product/hcm/Fap.Hcm.Web/Dockerfile .
 #docker run container,数据卷如下：-v 时区,-v logs,-v 附件。说明：设置appsetting.json日志路径为LogPath（/var/fapcore/logs），设置附件路径为（/var/fapcore/annex）
-#sudo docker run --name fapcorehcm -d -p 5000:80 -p 5001:443 -v /etc/localtime:/etc/localtime -v /usr/docker/fapcorehcm/logs:/var/fapcore/logs -v /usr/docker/fapcorehcm/annex:/var/fapcore/annex fapcore/hcm
-sudo docker build --pull -t fapcore/hcmapi -f product/hcm/Fap.Hcm.WebApi/Dockerfile .
-sudo docker run --name fapcorehcmapi -d -p 5002:80 -p 5003:443 -v /etc/localtime:/etc/localtime -v /usr/docker/fapcorehcm/logs:/var/fapcore/logs -v /usr/docker/fapcorehcm/annex:/var/fapcore/annex fapcore/hcmapi
+sudo docker run --name fapcorehcm -d -p 5000:80 -p 5001:443 -v /etc/localtime:/etc/localtime -v /usr/docker/fapcorehcm/logs:/var/fapcore/logs -v /usr/docker/fapcorehcm/annex:/var/fapcore/annex fapcore/hcm
+#sudo docker build --pull -t fapcore/hcmapi -f product/hcm/Fap.Hcm.WebApi/Dockerfile .
+#sudo docker run --name fapcorehcmapi -d -p 5002:80 -p 5003:443 -v /etc/localtime:/etc/localtime -v /usr/docker/fapcorehcm/logs:/var/fapcore/logs -v /usr/docker/fapcorehcm/annex:/var/fapcore/annex fapcore/hcmapi
 #rm Tag<none> images
 #docker rmi $(docker images | awk '/^<none>/ { print $3 }')
 
